e9e73d16be38716e0a9f70b1aa0fe27011d17875
yarisuli <148562678+yarisuli@users.noreply.github.com>
2024-06-25 22:03:52 -0300

    comparacion inputField con numero random

--- Assets/ControlScene.cs
+++ Assets/ControlScene.cs
@@ -8,13 +8,12 @@ public class ControlScene : MonoBehaviour
     public InputField inputNumero;
     public GameObject bola;
     public Spawnerbolas_segundo spawnerBolasScript;
-    
-
+    private int numerodbolas;
   
     // Start is called before the first frame update
     void Start()
     {
-        int numerodbolas = Random.Range(1, 5);
+        numerodbolas = Random.Range(1, 5);
         bola.SetActive(false);
         for (int i = 0; i < numerodbolas; i++)
         {
@@ -24,10 +23,22 @@ public class ControlScene : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CompararResultados()
     {
-        
+        if (inputNumero.text == "")
+        {
+            // mostrar en panel “Debes ingresar un resultado” 
+        }
+
+        else if (int.Parse(inputNumero.text) == numerodbolas)
+        {
+            // mostrar en el panel "Es correcto, felicitaciones!"
+        }
+        else
+        {
+            // mostrar en el panel "Es incorrecto."
+        }
+             
     }
 
 }
