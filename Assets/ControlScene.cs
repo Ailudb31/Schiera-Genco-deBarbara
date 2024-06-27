8657ad010ccc7ad5f58944027b60fc81eb771bfe
DE BARBARA AILEN <47872031@ort>
2024-06-27 15:17:02 -0300

    Arreglo general y de reinicio de escena cuando el número es incorrecto

--- Assets/ControlScene.cs
+++ Assets/ControlScene.cs
@@ -1,11 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.UIElements;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
-
 
 public class ControlScene : MonoBehaviour
 {
@@ -13,25 +10,13 @@ public class ControlScene : MonoBehaviour
     public GameObject BolaPrefab;
     public Spawnerbolas_segundo spawnerBolasScript;
     private int numeroBolas;
-    private int numeroBolasInicial;
     public Text txtPanelRta;
     public Text reiniciarTexto;
     public GameObject panelRespuesta;
 
     void Start()
     {
-        numeroBolas = Random.Range(0, 6);
-        //numeroBolasInicial = numeroBolas;
-        BolaPrefab.SetActive(false);
-
-        for (int i = 0; i < numeroBolas; i++)
-        {
-            spawnerBolasScript.CloneBola();
-        }
-        txtPanelRta.text = "";
-        Debug.Log(numeroBolas + 1);
-
-        //StartCoroutine(CaidaBolasConEspera());        
+        ReiniciarJuego();
     }
 
     public void CompararResultados()
@@ -45,6 +30,7 @@ public class ControlScene : MonoBehaviour
         {
             reiniciarTexto.text = "Reiniciar el desafio";
             txtPanelRta.text = "Es correcto, felicitaciones!";
+            StartCoroutine(ReiniciarEscena());
         }
         else
         {
@@ -53,36 +39,41 @@ public class ControlScene : MonoBehaviour
         }
     }
 
+    IEnumerator ReiniciarEscena()
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
     public void ReiniciarJuego()
     {
-        GameObject[] clones = GameObject.FindGameObjectsWithTag(BolaPrefab.name + "(Clone)");
+        numeroBolas = Random.Range(0, 6);
+        BolaPrefab.SetActive(false);
+
+        GameObject[] clones = GameObject.FindGameObjectsWithTag("BolaClone");
         foreach (GameObject clone in clones)
         {
             Destroy(clone);
         }
-        panelRespuesta.SetActive(false);
-        //StartCoroutine(CaidaBolasConEspera());
-        Start();
 
+        for (int i = 0; i < numeroBolas; i++)
+        {
+            spawnerBolasScript.CloneBola();
+        }
+        txtPanelRta.text = "";
+        Debug.Log(numeroBolas + 1);
+        panelRespuesta.SetActive(false);
     }
+
+    public void VolverIntentar() 
+    { 
+        panelRespuesta.SetActive(false); 
+    }
+
 
     public void SeleccionarJuego()
     {
         SceneManager.LoadScene("SeleccionarJuegos");
     }
-
-    //IEnumerator CaidaBolasConEspera()
-    //{
-    //    for (int i = 0; i < numeroBolas; i++)
-    //    {
-    //        spawnerBolasScript.CloneBola();
-    //    }
-    //    txtPanelRta.text = "";
-    //    Debug.Log(numeroBolas + 1);
-    //    yield return new WaitForSeconds(1f);
-    //}
-   
-  
 }
-
 
