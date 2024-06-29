916fd473c8468c74ef5a7f52ec4bd8a8fee84e9d
Thiago Schiera <schierathiago@gmail.com>
2024-06-29 19:07:43 -0300

    Escena NO carga otra vez cuando rta incorrecta
    (hubo ayuda de Iara)

--- Assets/ControlScene.cs
+++ Assets/ControlScene.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor.Experimental.UIElements.GraphView;
+
 
 public class ControlScene : MonoBehaviour
 {
-    public GameObject bolaprefab;
     public Vector3 newPosition;
     public float minX ;
     public float maxX;
     public float freq;
     public bool autoGenerate;
     
-    public Spawnerbolas_segundo spawnerBolasScript;
+    public GameObject objetoPrefab;
     public InputField inputNumero;
     public Text txtPanelRta;
     public Text reiniciarTexto;
-    public GameObject BolaPrefab;
     public GameObject panelRespuesta;
     private int numeroBolas;
 
@@ -25,7 +25,7 @@ public class ControlScene : MonoBehaviour
     void Start()
     {
         numeroBolas = Random.Range(0, 6);
-        BolaPrefab.SetActive(false);
+        objetoPrefab.SetActive(false);
 
         for (int i = 0; i <= (numeroBolas); i++)
         {
@@ -43,45 +43,38 @@ public class ControlScene : MonoBehaviour
         if (inputNumero.text == "")
         {
             reiniciarTexto.text = "Volver a intentarlo";
-            txtPanelRta.text = "Debes ingresar un resultado";
+            txtPanelRta.text = "Debes ingresar un resultado.";
         }
         else if (int.Parse(inputNumero.text) == (numeroBolas + 1))
         {
             reiniciarTexto.text = "Reiniciar el desafio";
-            txtPanelRta.text = "Es correcto, felicitaciones!";
-            //StartCoroutine(ReiniciarEscena());
+            txtPanelRta.text = "Es correcto!";
         }
         else
         {
             reiniciarTexto.text = "Volver a intentarlo";
-            txtPanelRta.text = "Es incorrecto. Intenta de nuevo.";
+            txtPanelRta.text = "Es incorrecto.       Intenta de nuevo.";
         }
     }
 
-    //IEnumerator ReiniciarEscena()
-    //{
-    //    yield return new WaitForSeconds(0.5f);
-    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    //}
-
-    public void ReiniciarJuego()
+    public void VolverIntentar()
     {
+        if (reiniciarTexto.text == "Volver a intentarlo")
+        {
+            GameObject[] clones = GameObject.FindGameObjectsWithTag("BolaClone");
+            foreach (GameObject clone in clones)
+            {
+                Destroy(clone);
+            }
 
-
-        //GameObject[] clones = GameObject.FindGameObjectsWithTag("BolaClone");
-        //foreach (GameObject clone in clones)
-        //{
-        //    Destroy(clone);
-        //}
-
-        Start();
+            Start();
+        }
+        
+        else
+        {
+           SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
-
-    public void VolverIntentar() 
-    { 
-        panelRespuesta.SetActive(false ); 
-    }
-
 
     public void SeleccionarJuego()
     {
@@ -90,10 +83,11 @@ public class ControlScene : MonoBehaviour
 
     public void CloneBola()
     {
-        bolaprefab.SetActive(true);
+        objetoPrefab.SetActive(true);
         float randomX = Random.Range(minX, maxX);
         newPosition = new Vector3(randomX, newPosition.y, newPosition.z);
-        Instantiate(bolaprefab, newPosition, Quaternion.Euler(-90, 0, 0));
+        GameObject clone = Instantiate(objetoPrefab, newPosition, Quaternion.Euler(-90, 0, 0));
+        clone.tag = "BolaClone";
     }
 }
 
